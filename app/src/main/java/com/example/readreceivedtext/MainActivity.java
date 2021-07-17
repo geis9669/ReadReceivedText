8fe00755180388f9b9d40cdff7d90d39f464e145
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-07-17 17:45:04 -0600

    made it show the text messages in a toast

--- app/src/main/java/com/example/readreceivedtext/MainActivity.java
+++ app/src/main/java/com/example/readreceivedtext/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.readreceivedtext;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int Request_Permission = 1;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.RECEIVE_SMS}, Request_Permission);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Has Permissions", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/readreceivedtext/SmsReceiver.java
@@ -0,0 +1,74 @@
+package com.example.readreceivedtext;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+    public static final String SMS_BUNDLE = "pdus";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        Toast.makeText(context, "Start OnReceive", Toast.LENGTH_LONG).show();
+
+        Bundle intentExtras = intent.getExtras();
+        if(intentExtras != null){
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+            for(int i=0; i<sms.length; i++){
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+                String smsBody = smsMessage.getMessageBody().toString();
+                String address = smsMessage.getOriginatingAddress();
+                smsMessageStr += "SMS From: " + address + "\n";
+                smsMessageStr += smsBody + "\n";
+            }
+            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
+
+            //this will update the UI with message
+            SmsActivity inst = SmsActivity.instance();
+            inst.updateList(smsMessageStr);
+        }
+
+
+        // Get the SMS message
+//        Bundle bundle = intent.getExtras();
+//        SmsMessage[] msgs;
+//        String strMessage = "";
+//        String format = bundle.getString("format");
+//        // Retrieve the SMS message received.
+//        Object[] pdus = (Object[]) bundle.get("pdus");
+//        if(pdus != null) {
+//            // Check the Android version.
+//            boolean isVersionM = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+//            //Fill the msgs array.
+//            msgs = new SmsMessage[pdus.length];
+//            for(int i = 0; i<msgs.length; i++){
+//                // Check Android version and use appropriate create From Pdu.
+//                if(isVersionM)
+//                {
+//                    //If Android version M or newer:
+//                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+//                } else {
+//                    // If Android version L or older:
+//                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+//                }
+//                // Build the message to show.
+//                strMessage += "SMS from " + msgs[i].getOriginatingAddress();
+//                strMessage += " :" + msgs[i].getMessageBody() +"\n";
+//                // Log and display the SMS message.
+//                Log.d("ReadReceivedText", "onReceive: " + strMessage);
+//                Toast.makeText(context, strMessage, Toast.LENGTH_LONG).show();
+//            }
+//        }
+
+    }
+}
